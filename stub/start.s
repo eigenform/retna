9614c297ac191cd1d9fbbaf3ecb8b406118ad165
meta <eigenform@users.noreply.github.com>
2021-01-22 15:42:14 -0600

    stub: tiny cleanup

--- stub/start.s
+++ stub/start.s
@@ -5,9 +5,9 @@
 .global _start
 _start:
 	// We enter this code from Thumb mode, so switch back to ARM
-	add	r1, pc, #0x0c
+	ldr	r1, =__arm_start
 	bx	r1
-.word	0, 0, 0
+.ltorg
 
 .arm
 __arm_start:
@@ -17,15 +17,14 @@ __arm_start:
 	ldr	r1, =0x20100869
 	str	r1, [sp, #-4]
 
-__main:
 	bl	main
 
-__arm_exit:
 	// Restore the original LR we clobbered, return with -1337
 	ldr	r0, =0xfffffac7	
 	ldr	r3, =0x20100869
 	mov	lr, r3
 	bx	lr
+.ltorg
 
 .global boot_new_ios_kernel
 __syscall_boot_new_ios_kernel:
